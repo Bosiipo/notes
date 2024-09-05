29fcccfe265c055a0c9fe5be500179ac551350aa
Bosipo <bosiposhogbolu@gmail.com>
2024-09-05 22:45:12 +0100

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,84 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import NavBar from './NavBar'
-import CreateArea from './CreateArea'
-import { useState } from 'react';
-import Note from './Note'
+import type { NextPage } from "next";
+import Head from "next/head";
+import NavBar from "./NavBar";
+import CreateArea from "./CreateArea";
+import { useEffect, useState } from "react";
+import Note from "./Note";
+import dotenv from "dotenv";
+dotenv.config();
 
+type Note = {
+  _id: string;
+  title: string;
+  content: string;
+};
 
 const Home: NextPage = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 
-  const [notes, setNotes] = useState<string[]>([])
-
-  function addNote(newnote:any){
-    setNotes((preNotes)=>{
-      return [...preNotes,newnote]
-    })
-
+  const callNotes = async () => {
+    try {
+      let res = await fetch(baseUrl || "")
+        .then((res) => res.json())
+        .then((response) => {
+          console.log({ response });
+          if (response.status) {
+            setNotes(response.data);
+          }
+        })
+        .catch((e) => console.log({ e }));
+    } catch (error) {
+      console.log({ error });
     }
-  function deleteNote(id:any){
-    setNotes(preNotes=>{
-      return preNotes.filter((noteItem,index)=>{
-        return index !== id;
-      })
-    })
-  }
+  };
 
-  
-    
-    
+  useEffect(() => {
+    callNotes();
+  }, []);
+
+  const addNote = async (newNote: any) => {
+    try {
+      await fetch(baseUrl || "", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newNote),
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          if (response.status) {
+            setNotes((preNotes) => {
+              return [...preNotes, response.data];
+            });
+          }
+        })
+        .catch((e) => console.log({ e }));
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+  const deleteNote = async (id: any) => {
+    await fetch(`${baseUrl}/${id._id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        if (response.status) {
+          setNotes((preNotes) => {
+            return preNotes.filter((noteItem, index) => {
+              return noteItem._id !== id;
+            });
+          });
+          callNotes();
+        }
+      })
+      .catch((e) => console.log({ e }));
+  };
 
   return (
     <div>
@@ -35,16 +87,22 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <NavBar text='Notes' />
+      <NavBar text="Notes" />
       <CreateArea onAdd={addNote} />
 
-    
-     {notes.map((noteItem:any,index)=>{ return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}  />
- })}
-
-      
+      {notes.map((noteItem: any, index) => {
+        return (
+          <Note
+            key={index}
+            id={noteItem}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Home;
